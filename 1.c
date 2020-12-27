7bb1127c10ceada809e0df45f007d6b06108b3db
王千楠 <57887647+wang2018110733@users.noreply.github.com>
2020-12-27 22:44:58 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,22 +1,5 @@
 #include<stdio.h>
 int main()
 {
-  intx,temp;
-  for(x=1;x<100;x++)
-  {
-    sum=0;
-    for(temp=1;temp<x;temp++)
-    {
-      if(x%temp==0)
-      {
-        sum=sum+temp;
-      }
-    }
-    if(sum==x)
-    {
-      printf("%d",x);
-    }
-  }
-  printf("\n");
   return 0;
 }
